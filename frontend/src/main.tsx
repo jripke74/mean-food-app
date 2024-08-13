e4cc00c2fb761db69a7561c9cfb57612e9707305
Jeff Ripke <jripke74@gmail.com>
2024-08-13 09:21:47 -0700

    add layout.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './global.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from './AppRoutes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router></Router>
+    <Router>
+      <AppRoutes />
+    </Router>
   </StrictMode>,
-)
+);
